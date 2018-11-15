9f7269df0647135a87b96d82abb11aed3f3a2026
NikolaiRojahn <cph-nr87@cphbusiness.dk>
2018-11-15 18:12:58 +0100

    countScrewsForRafters and UnitTest of all our calculator methods

--- src/main/java/FunctionLayer/Calculator.java
+++ src/main/java/FunctionLayer/Calculator.java
@@ -27,34 +27,44 @@ public class Calculator {
     public int countScrewsForBrackets(Carport carport) {
         int amountOfScrewsPerBracket = 8;
         int brackets = countBracketsForCarport(carport);
-        int screws = (brackets * amountOfScrewsPerBracket); // Total amount of screws. For each brackets, 8 screws are added
-        
+        int screws = (brackets * amountOfScrewsPerBracket); // Total amount of screws for the brackets. For each brackets, 8 screws are added
+
         return screws;
     }
 
-    public int countRemForCarport(Carport carport) {   
+    public int countScrewsForRaftersFlatRoof(Carport carport) {
+        int amountOfScrewsForOneMeterRaft = 4;
+        double totalRafterMeters = Math.ceil(countRaftersForRoof(carport) * carport.getWidth()); // Total amount of rafter meters. All counted rafters are multiplied with the width of the carport.
+        int screws = (int) totalRafterMeters * amountOfScrewsForOneMeterRaft; // Total amount of screws for the rafters, to hold the flat roof. For each rafter meter, 4 screws are added.
+
+        return screws;
+    }
+
+    public int countRemForCarport(Carport carport) {
         int amountOfRemFlatRoof = 2;
         int amountOfRemNotFlatRoof = 4;
-        if (!carport.getRoof().isFlatRoof()) return amountOfRemNotFlatRoof; // Amount of remme is set to 4 if the roof is not flat
+        if (!carport.getRoof().isFlatRoof()) {
+            return amountOfRemNotFlatRoof; // Amount of remme is set to 4 if the roof is not flat
+        }
         return amountOfRemFlatRoof; // Amount of remme is set to 2 if the roof is flat
     }
-    
+
     public void calculateRoofDimensions(Carport carport) {
         double extraSpaceFlatRoof = 0.10;
-        if (carport.getRoof().isFlatRoof()) {    
-        double extraLength = extraSpaceFlatRoof * 2; // Total count of extra space for roof. For each length it would be added 10 cm (* 2)
-        double extraWidth = extraSpaceFlatRoof * 2; // Total count of extra space for roof. For each wide it would be added 10 cm (* 2)
-        carport.getRoof().setLength(carport.getLength() + extraLength);
-        carport.getRoof().setWidth(carport.getWidth() + extraWidth);
+        if (carport.getRoof().isFlatRoof()) {
+            double extraLength = extraSpaceFlatRoof * 2; // Total count of extra space for roof. For each length it would be added 10 cm (* 2)
+            double extraWidth = extraSpaceFlatRoof * 2; // Total count of extra space for roof. For each wide it would be added 10 cm (* 2)
+            carport.getRoof().setLength(carport.getLength() + extraLength);
+            carport.getRoof().setWidth(carport.getWidth() + extraWidth);
         }
     }
-    
+
     public int countRaftersForRoof(Carport carport) {
         double carportLength = carport.getLength();
         double spaceBetweenEachRafter = 0.59;
         double rafterWidth = 0.02;
         double totalRafterDimension = spaceBetweenEachRafter + rafterWidth; // Total dimension for each rafter including both space and material.
-                
+
         double rafters = Math.ceil(carportLength / totalRafterDimension); // Total amount of rafter based on calculating length with total dimension per rafter
         return (int) rafters;
     }
