29adf4eae492a81c1aebaa8dbad4f54db69be4ba
NikolaiRojahn <cph-nr87@cphbusiness.dk>
2018-12-04 09:37:15 +0100

    adding two small changes to register class

--- src/main/java/PresentationLayer/Register.java
+++ src/main/java/PresentationLayer/Register.java
@@ -22,19 +22,19 @@ import javax.servlet.http.HttpSession;
 
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws LoginUserException {
-        String zipcode = request.getParameter("zipcode");
-        String phone = request.getParameter("phone");
         String firstName = request.getParameter("firstname");
         String lastName = request.getParameter("lastname");
+        String zipcode = request.getParameter("zipcode");
         String city = request.getParameter("city");
         String email = request.getParameter("email");
-        String role = request.getParameter("role");
+        String phone = request.getParameter("phone");
         String password1 = request.getParameter("password1");
         String password2 = request.getParameter("password2");
+        String role = request.getParameter("role");
         if (password1.equals(password2)) {
             Customer customer = null;
             try {
-                customer = LogicFacade.createCustomer(Integer.parseInt(zipcode), Integer.parseInt(phone), firstName, lastName, city, email, password1, role);
+                customer = LogicFacade.createCustomer(firstName, lastName, email, Integer.parseInt(zipcode), city, Integer.parseInt(phone), password2, role);
             } catch (InstantiationException ex) {
                 Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
             } catch (IllegalAccessException ex) {
